c1929af5342c01a8d024b9aa1898f80a876928ad
yariks5s <yaroslav.briukhovetskyi@clickhouse.com>
2023-09-16 22:15:18 +0000

    test fix

--- tests/accessibility_test.py
+++ tests/accessibility_test.py
@@ -7,7 +7,3 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/is_up")
     assert response.status_code == 200
-
-def test_check_default_news():
-    response = client.get("/get_news/1")
-    assert response.status_code == 200
